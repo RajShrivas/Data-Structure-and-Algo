d77862173211905b331a3ea3a9bd5f3330f58f1a
Raj <raj@gmail.com>
2022-07-19 16:44:46 +0530

    adding files

---
+++ src/com/startk/array/IntroArray.java
@@ -0,0 +1,30 @@
+package com.startk.array;
+
+import java.util.Arrays;
+
+public class IntroArray {
+
+    public static void main(String[] args) {
+
+        //syntax
+        //dataType[] var_name = new dataType[size];
+        int[] arr = new int[5];
+        arr[0] = 5;
+        arr[3] = 9;
+
+        int[] num = {5, 77, 23, 55, 99, 443};
+
+        System.out.println(Arrays.toString(arr));
+
+
+        // print with enchanced for each loop
+        for (int n : num) {
+            System.out.println(n);
+        }
+
+        // print with simple for loop
+        for (int i = 0; i < num.length; i++) {
+            System.out.println(num[i]);
+        }
+    }
+}
