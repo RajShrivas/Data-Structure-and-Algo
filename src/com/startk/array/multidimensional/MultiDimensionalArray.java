62d084747b626a0d13d077b2171e15859e60b6e3
Raj <raj@gmail.com>
2022-07-25 18:13:56 +0530

    updated multiArray file

--- src/com/startk/array/multidimensional/MultiDimensionalArray.java
+++ src/com/startk/array/multidimensional/MultiDimensionalArray.java
@@ -1,4 +1,55 @@
 package com.startk.array.multidimensional;
 
 public class MultiDimensionalArray {
+
+    static int input;
+
+    public static void main(String[] args) {
+
+        int[][] arr = new int[3][];
+
+        int[][] arr2D = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+
+        printOutput(arr2D);
+
+
+
+    }
+
+
+    public static void printOutput(int[][] arr){
+ /*       for (int row = 0; row < arr.length ; row++) {
+
+            for (int col = 0; col < arr[row].length; col++) {
+
+                input= arr[row][col] ;
+                System.out.print(input + " ");
+            }
+
+            System.out.println();
+
+        }*/
+
+
+        //Another approach
+
+        for (int row = 0; row < arr.length ; row++) {
+            int[] ar = arr[row];
+
+            for (int col = 0; col < ar.length; col++) {
+
+                int r = ar[col];
+
+                System.out.print(r +" ");
+            }
+
+            System.out.println();
+        }
+
+
+    }
 }
