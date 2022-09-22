2b9059213dc1c88e900ad1c99f42a9a49f6e7a17
Raj <raj@gmail.com>
2022-09-22 14:36:40 +0530

    leetcode greaterLetter problem correction ::22 Sep 2022

--- src/com/stark/leetcode/GreaterLetterThanTarget.java
+++ src/com/stark/leetcode/GreaterLetterThanTarget.java
@@ -4,13 +4,16 @@ public class GreaterLetterThanTarget {
 
     public static void main(String[] args) {
         char[] arr = new char[]{'c', 'f', 'k'};
-        char target = 'l';
+        char target = 'g';
         int result = greaterLetterThanTarget(arr, target);
         char r = (char) result;
         System.out.println(r);
     }
 
     private static char greaterLetterThanTarget(char[] arr, char target) {
+        if (arr.length == 0) {
+            return 'n';
+        }
         int start = 0;
         int end = arr.length - 1;
 
@@ -21,6 +24,8 @@ public class GreaterLetterThanTarget {
                 end = mid - 1;
             } else if (target > value) {
                 start = mid + 1;
+            }else {
+                return arr[mid];
             }
         }
         return arr[start % arr.length];
