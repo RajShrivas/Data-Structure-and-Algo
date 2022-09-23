3bc3c4e3c54fbffea905e14cfd4369e479df66ed
Raj <raj@gmail.com>
2022-09-23 23:32:30 +0530

    Insertion Sort Introduction with Code ::23 Sep 2022

---
+++ src/com/stark/sorting/insertionSort/Intro.java
@@ -0,0 +1,30 @@
+package com.stark.sorting.insertionSort;
+
+import java.util.Arrays;
+
+public class Intro {
+
+    public static void main(String[] args) {
+        int[] arr = {3, 1, 12, 11, 13};
+        insertionSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void insertionSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j > 0; j--) {
+                if (arr[j] < arr[j - 1]) {
+                    swap(arr, j, j - 1);
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+
+    private static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
