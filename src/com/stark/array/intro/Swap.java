bead2e96cc17642cdb02fefdb2ddf66ec2e04654
Raj <raj@gmail.com>
2022-09-28 17:01:22 +0530

    Spaces removed ::28 Sep 2022

--- src/com/stark/array/intro/Swap.java
+++ src/com/stark/array/intro/Swap.java
@@ -5,22 +5,17 @@ import java.util.Arrays;
 public class Swap {
 
     public static void main(String[] args) {
-
         int[] arr = {1, 3, 6, 7, 9, 10};
-       int[] a= swap(arr, 0, 3);
-
+        int index1 = 0;
+        int index2 = 3;
+        int[] a = swap(arr, index1, index2);
         System.out.println(Arrays.toString(a));
-
     }
 
     private static int[] swap(int[] arr, int index1, int index2) {
-
         int temp = arr[index1];
-
         arr[index1] = arr[index2];
         arr[index2] = temp;
-
         return arr;
-
     }
 }
