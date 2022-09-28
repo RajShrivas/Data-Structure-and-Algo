a09697c1f2442dcdaeee25525ef084ed84357439
Raj <raj@gmail.com>
2022-09-28 16:26:08 +0530

    Cyclic Sort Introduction with Code ::28 Sep 2022

---
+++ src/com/stark/sorting/cyclicSort/Intro.java
@@ -0,0 +1,28 @@
+package com.stark.sorting.cyclicSort;
+
+import java.util.Arrays;
+
+public class Intro {
+
+    public static void main(String[] args) {
+        int[] arr = {3, 1, 4, 2, 5, 7, 6, 8};
+        cyclicSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    private static void cyclicSort(int[] arr) {
+        int i = 0;
+        while (i < arr.length) {
+            int correct = arr[i] - 1;
+            if (arr[i] != arr[correct]) {
+                swap(arr, i, correct);
+            } else {
+                i++;
+            }
+        }
+    }
+    private static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
